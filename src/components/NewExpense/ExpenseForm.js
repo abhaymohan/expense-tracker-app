7b87bf5187e52d3e53957e1c76eb64ae0bc58c63
abhay mohan <abhayms2018@gmail.com>
2022-07-18 15:00:08 +0530

    added chart , chartbar, submit form and button with dynamic styling

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -45,7 +45,7 @@ const ExpenseForm = (props) => {
     event.preventDefault();
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
     props.onSaveExpenseData(expenseData);
@@ -83,6 +83,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions"></div>
+      <button type="button" onClick={props.onCancel}>Cancel</button>
       <button type="submit">Add Expense</button>
     </form>
   );
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import ExpenseForm from './ExpenseForm';
 
 import './NewExpense.css';
 
 const NewExpense = (props) =>{
+
+    const [isEditing, setIsEditing] = useState(false);
 
     const saveExpenseDataHandler = (enteredExpenseData) => 
     {
@@ -13,10 +15,20 @@ const NewExpense = (props) =>{
             id: Math.random().toString()
         };
         props.onAddExpense(expenseData);
-    } 
+        setIsEditing(false);
+    };
+    
+    const stopEditingHandler = () => {
+        setIsEditing(false);
+    }
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    }
+
 
     return <div className='new-expense'>
-        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+        {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+        {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler}/>}
     </div>
 };
 
